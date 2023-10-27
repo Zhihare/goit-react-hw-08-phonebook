5f965fd3e8a7d9b48b07b254b606fa8814cefa8a
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-10-27 14:26:49 +0300

    start

--- src/index.js
+++ src/index.js
@@ -8,26 +8,29 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light" />
-      </PersistGate>
-    </Provider>
+    <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light" />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 
 );
